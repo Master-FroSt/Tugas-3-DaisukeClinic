89f8de43c0105c1fdf39e01c8500fa881c1c9bd5
Master-Frost <mfschannel30@gmail.com>
2025-05-28 21:32:21 +0700

    Main Menu dan LinkedLIst

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,7 +1,293 @@
-public class LinkedList {
+class Node<T> {
+    // Atribute
+    T value;
+    Node<T> nextNode;
+    Node<T> prevNode;
 
+    // Construstor
+    Node(T value) {
+        this.value = value;
+        this.nextNode = null;
+        this.prevNode = null;
+    }
+}
 
-    public LinkedList(){} //constructor
+public class LinkedList<T> {
+    // Atribute
+    private Node<T> head = null;
+    private Node<T> tail = null;
+    private int size = 0;
 
-    //Tambah method addFirst, remove, search, display/traversal, size, addLast, removeFirst, get, isEmpty,
+    // Method to get size
+    public int size() {
+        return size;
+    }
+
+    // method to check if LinkedList empty
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // Method to empty-ing LinkedList
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    //Method to print LinkedList from forward (head)
+    public void printForward() {
+        if (isEmpty()) {
+            System.out.println("LinkedList is Empty");
+        }
+        else {
+            Node<T> temp = head;
+            while (temp != null) {
+                System.out.print(temp.value + " <-> ");
+                temp = temp.nextNode;
+            }
+            System.out.println("null");
+        }
+    }
+
+    // Method to print LinkedList from backward (tail)
+    public void printBackward() {
+        if (isEmpty()) {
+            System.out.println("LinkedList is Empty");
+        }
+        else {
+            Node<T> temp = tail;
+            while (temp != null) {
+                System.out.print(temp.value + " <-> ");
+                temp = temp.prevNode;
+            }
+            System.out.println("null");
+        }
+    }
+
+    // Method to add node at the beginning
+    public void addFirst(T value) {
+        Node<T> newNode = new Node<>(value);
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            head.prevNode = newNode;
+            newNode.nextNode = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    // Method to add node at the end
+    public void addLast(T value) {
+        Node<T> newNode = new Node<>(value);
+        if (isEmpty()) {
+            head = newNode;
+        }
+        else {
+            tail.nextNode = newNode;
+            newNode.prevNode = tail;
+        }
+        tail = newNode;
+        size++;
+    }
+
+    // Method to add node at spesific index
+    public void addAt(int index, T value) {
+        if (index <= 0) {
+            addFirst(value);
+            return;
+        }
+        else if (index >= size) {
+            addLast(value);
+            return;
+        }
+
+        Node<T> newNode = new Node<>(value);
+        if (index <= size / 2) {
+            Node<T> temp = head;
+            for (int i = 0; i < index; i++) {
+                temp = temp.nextNode;
+            }
+            newNode.nextNode = temp;
+            newNode.prevNode = temp.prevNode;
+            temp.prevNode = newNode;
+            newNode.prevNode.nextNode = newNode;
+        }
+        else {
+            Node<T> temp = tail;
+            for (int i = size - 1; i > index; i--) {
+                temp = temp.prevNode;
+            }
+            newNode.nextNode = temp;
+            newNode.prevNode = temp.prevNode;
+            temp.prevNode = newNode;
+            if (newNode.prevNode != null) {
+                newNode.prevNode.nextNode = newNode;
+            }
+        }
+        size++;
+    }
+
+    // Method to set (replace) value at a specific index
+    public void set(int index, T newValue) {
+        if (index < 0 || index >= size) {
+            System.out.println("Index out of bounds");
+            return;
+        }
+
+        Node<T> temp;
+        if (index < size / 2) {
+            temp = head;
+            for (int i = 0; i < index; i++) {
+                temp = temp.nextNode;
+            }
+        } else {
+            temp = tail;
+            for (int i = size - 1; i > index; i--) {
+                temp = temp.prevNode;
+            }
+        }
+
+        temp.value = newValue;
+    }
+
+    // Method to remove the first node
+    public void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("LinkedList is Empty");
+            return;
+        }
+        if (size == 1) {
+            head = null;
+            tail = null;
+        }
+        else {
+            head = head.nextNode;
+            head.prevNode = null;
+        }
+        size--;
+    }
+
+    // Method to remove the last node
+    public void removeLast() {
+        if (isEmpty()) {
+            System.out.println("LinkedList is Empty");
+            return;
+        }
+        if (size == 1) {
+            head = null;
+            tail = null;
+        }
+        else {
+            tail = tail.prevNode;
+            tail.nextNode = null;
+        }
+        size--;
+    }
+
+    // Method to remove node at spesific index
+    public void removeAt(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("Index not found");
+            return;
+        }
+        if (index == 0) {
+            removeFirst();
+            return;
+        }
+        if (index == size - 1) {
+            removeLast();
+            return;
+        }
+
+        Node<T> temp;
+        if (index <= size / 2) {
+            temp = head;
+            for (int i = 0; i < index; i++) {
+                temp = temp.nextNode;
+            }
+        }
+        else {
+            temp = tail;
+            for (int i = size - 1; i > index; i--) {
+                temp = temp.prevNode;
+            }
+        }
+        temp.prevNode.nextNode = temp.nextNode;
+        temp.nextNode.prevNode = temp.prevNode;
+        size--;
+    }
+
+    // Method to remove node with spesific value
+    public boolean removeByValue(T value) {
+        int index = indexOf(value);
+        if (index < 0) {
+            System.out.println("Value not be found");
+            return false;
+        }
+        removeAt(index);
+        return true;
+    }
+
+    // Method to get value at spesific index
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("Index not found");
+            return null;
+        }
+        Node<T> temp;
+        if (index < size / 2) {
+            temp = head;
+            for (int i = 0; i < index; i++) {
+                temp = temp.nextNode;
+            }
+        }
+        else {
+            temp = tail;
+            for (int i = size - 1; i > index; i--) {
+                temp = temp.prevNode;
+            }
+        }
+        return temp.value;
+    }
+
+    // Method to get the index of a spesific given value
+    public int indexOf(T value) {
+        Node<T> temp = head;
+        int index = 0;
+        while (temp != null) {
+            if (temp.value.equals(value)) {
+                return index;
+            }
+            temp = temp.nextNode;
+            index++;
+        }
+        return -1; // Not found
+    }
+
+    // Method to check if value exists in LinkedList
+    public boolean contains(T value) {
+        return indexOf(value) != -1;
+    }
+
+    // Method to get the value of the first Node
+    public T getFirst() {
+        if (isEmpty()) {
+            System.out.println("LinkedList is empty");
+            return null;
+        }
+        return head.value;
+    }
+
+    // Method to get the value of the last Node
+    public T getLast() {
+        if (isEmpty()) {
+            System.out.println("LinkedList is empty");
+            return null;
+        }
+        return tail.value;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,81 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello, World!");
-        //Menu
-        //==== Data Manager Pro ====
-        //1. Add New Patient
-        //2. Remove Patient by ID
-        //3. Search Patient by Name
-        //4. Display All Patients
-        //5. Doctor Login
-        //6. Doctor Logout
-        //7. View Last Logged-in Doctor
-        //8. Schedule Appointment
-        //9. Process Appointment
-        //10. Display Upcoming Appointments
-        //11. Search Patient by ID (BST)
-        //12. Display All Patients (BST Inorder)
-        //0. Exit
-        //==========================
+        while (true){
+            System.out.println("=== Sistem Rumah Sakit ===");
+            System.out.println("1. Mendaftar Pasien");
+            System.out.println("2. Hapus Pasien: ");
+            System.out.println("3. Cari Pasien: ");//ini juga sama aja ga sih? ntar kita kasih menu lagi?
+            System.out.println("4. Tampilkan semua Pasien"); //mending disini aja ntar dikasih pilihan ga si, daripada ada 2 menu yang sama, jadi di case kita ada scanner buat menu pake BST inorder sama yang ga
+            System.out.println("5. login Dokter");
+            System.out.println("6. logout Dokter: ");// cari ID
+            System.out.println("7. Daftar Dokter: ");//pake stack?
+            System.out.println("8. Jadwal reservasi: ");
+            System.out.println("9. Proses reservasi: ");
+            System.out.println("10. Daftar reservasi: ");
+            System.out.println("11. Keluar");
+            System.out.print("Pilih menu: ");
+            int pilihan = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    // Logika untuk mendaftar pasien
+                    System.out.println("Mendaftar Pasien...");
+                    // Tambahkan kode untuk mendaftar pasien
+                    break;
+                case 2:
+                    // Logika untuk menghapus pasien
+                    System.out.println("Menghapus Pasien...");
+                    // Tambahkan kode untuk menghapus pasien
+                    break;
+                case 3:
+                    // ntar disini manggil satu class aja tapi didalam class itu ada pilihannya.
+                    break;
+                case 4:
+                    //sama aja kek case 3.
+                    break;
+                case 5:
+                    // Logika untuk login dokter
+                    System.out.println("Login Dokter...");
+                    // Tambahkan kode untuk login dokter
+                    break;
+                case 6:
+                    // Logika untuk logout dokter
+                    System.out.println("Logout Dokter...");
+                    // Tambahkan kode untuk logout dokter
+                    break;
+                case 7:
+                    // Logika untuk daftar dokter
+                    System.out.println("Daftar Dokter...");
+                    // Tambahkan kode untuk daftar dokter
+                    break;
+                case 8:
+                    // Logika untuk jadwal reservasi
+                    System.out.println("Jadwal Reservasi...");
+                    // Tambahkan kode untuk jadwal reservasi
+                    break;
+                case 9:
+                    // Logika untuk proses reservasi
+                    System.out.println("Proses Reservasi...");
+                    // Tambahkan kode untuk proses reservasi
+                    break;
+                case 10:
+                    // Logika untuk daftar reservasi
+                    System.out.println("Daftar Reservasi...");
+                    // Tambahkan kode untuk daftar reservasi
+                    break;
+                case 11:
+                    System.out.println("Keluar dari sistem.");
+                    scanner.close();
+                    return; // Keluar dari program
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan coba lagi.");
+            }
+            scanner.close();
+        }
     }
 }
